b5375ca039131502444bd8b078bbb809157ce580
Tom Coxon <tom.c.coxon@googlemail.com>
2015-04-18 23:33:49 +0100

    Fix sprite positioning bug.

--- source/NiceSprite.hx
+++ source/NiceSprite.hx
@@ -5,12 +5,12 @@ import flixel.*;
 class NiceSprite extends FlxSprite {
 
   public function getPoint(): Vec2 {
-    return new Vec2(x+origin.x, y+origin.x);
+    return new Vec2(x+origin.x-offset.x, y+origin.x-offset.y);
   }
 
   public function setPoint(point: Vec2) {
-    this.x = point.x-origin.x;
-    this.y = point.y-origin.x;
+    this.x = point.x-origin.x+offset.x;
+    this.y = point.y-origin.x+offset.y;
   }
 
   public function addToPoint(vec: Vec2) {
