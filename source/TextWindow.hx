dc3613e76133c50da0095e6a2e59829c88ae1c02
Tom Coxon <tom.c.coxon@googlemail.com>
2015-04-18 20:15:02 +0100

    Fix textbox height.

--- source/TextWindow.hx
+++ source/TextWindow.hx
@@ -10,21 +10,22 @@ class TextWindow extends FlxGroup {
   private var background: FlxSprite;
   private var clickIndicator: SimpleAnimation;
   private var mouseWasPressed: Bool = false;
-  private var offset: Vec2;
+  private var inset: Vec2;
+  private var textInset: Vec2;
   private var size: Vec2;
+  private var color: UInt;
 
   public function new(text: String, ?color: UInt=0xff000000) {
     super();
-    offset = new Vec2(8,8);
-    var width = FlxG.width - offset.x*2;
+    inset = new Vec2(8,8);
+    textInset = new Vec2(4,4);
+    this.color = color;
+    var width = FlxG.width - inset.x*2;
 
-    textObj = new FlxText(0, 0, width, text);
+    textObj = new FlxText(0, 0, width - textInset.x*2, text);
     textObj.font = "assets/Beeb.ttf";
 
-    size = new Vec2(width, textObj.height);
-
     background = new FlxSprite(0, 0);
-    background.makeGraphic(Std.int(size.x), Std.int(size.y), color);
 
     clickIndicator = new SimpleAnimation("click-indicator");
 
@@ -33,10 +34,21 @@ class TextWindow extends FlxGroup {
     add(clickIndicator);
   }
 
+  override public function draw() {
+    super.draw();
+    if (size == null) {
+      var width = FlxG.width - inset.x*2;
+      size = new Vec2(width, textObj.region.height + textInset.y*2);
+      background.makeGraphic(Std.int(size.x), Std.int(size.y), color);
+    }
+  }
+
   private function setPosition(x: Float, y: Float) {
-    x += offset.x;
-    y += offset.y;
-    textObj.setPosition(x, y);
+    if (size == null)
+      return;
+    x += inset.x;
+    y += inset.y;
+    textObj.setPosition(x + textInset.x, y + textInset.y);
     background.setPosition(x, y);
     clickIndicator.setPosition(x + size.x - clickIndicator.width - 1, y + size.y - clickIndicator.height - 1);
   }
